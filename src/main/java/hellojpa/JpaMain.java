9c0beb06c491e349fec60e20f2dfeebf7a950633
Gwancheol-Lee <rhkscjf9785@naver.com>
2023-02-21 23:06:20 +0900

    ãTest simple query. insert, select, update with JPA

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,21 +9,33 @@ public class JpaMain {
 
 	public static void main(String[] args) {
 		// EntityManagerFactory를 통해 DB를 연결 ( hello는 persistence.xml의 persistence-unit name ) 
-		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello"); // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
 		
 		// createEntityManager() 메소드를 통해 생성된 EntityManger로 SQL 작업.
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = emf.createEntityManager(); // 엔티티 매니저는 쓰레드간에 공유하지 않음. 해당 트랜잭션에만 사용하고 close
 		
 		// 데이터를 변경하는 작업에는 꼭 Transaction이 필요하다.
-		EntityTransaction tx = em.getTransaction();	
+		EntityTransaction tx = em.getTransaction();	// JPA 모든 데이터 변경은 트랜잭션 안에서 실행됨.
 		
 		try {
 			tx.begin(); // SQL 실행 코드 전에 트랜잭션 begin 선언
 			
+			// 1) 등록
+			/*
 			Member member = new Member();
 			member.setId(2L);
 			member.setName("HelloB");
 			em.persist(member); // Member 테이블에 데이터 Insert
+			*/
+			
+			// 2) 조회
+			Member findMember = em.find(Member.class, 1L); // EntityClass 와 PK ID로 Member 조회
+			System.out.println("findMember.id = " + findMember.getId());
+			System.out.println("findMember.Name = " + findMember.getName());
+			
+			// 3) 수정
+			// 자바 컬렉션과 같은 개념으로 setName으로 데이터를 변경하면 JPA에서 커밋 시점 전에 체크하고 DB에 업데이트 쿼리를 날림. 개쩐다.
+			findMember.setName("HelloJPA"); 
 			
 			tx.commit(); // SQL 실행 코드 이후에 트랜잭션 commit
 		} catch (Exception e) {
