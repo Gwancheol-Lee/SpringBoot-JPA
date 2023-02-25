6a2fb8cc10e93106073210379ba9dfd02b55307a
Gwancheol-Lee <rhkscjf9785@naver.com>
2023-02-25 13:04:39 +0900

    Practice JPA Example

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+	@Id @GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+	@Column(name = "MEMBER_ID")
+	private Long memberId;
+	private Member member;
+	private LocalDateTime orderDate;
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
+}
---
+++ src/main/java/jpabook/jpashop/main/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.main;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import jpabook.jpashop.domain.Order;
+
+public class JpaMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		try {
+			tx.begin();
+			
+			Order order = em.find(Order.class, 1L);
+			Long memberId = order.getMemberId();
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		
+		emf.close();
+	}
+
+}
